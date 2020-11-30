64208f1fc2d013bbd688f8aa4c8a1e9f1742ca8c
Abhinav <abhinav7693ngh@gmail.com>
2020-12-01 02:07:16 +0530

    About logging levels

--- logger.js
+++ logger.js
@@ -5,6 +5,21 @@ const level = process.env.NODE_LOGGING_LEVEL || 'info';
 
 // console.log('logging level ', level);
 
+// ======== Different levels of logs ========== //
+/*
+    1. Fatal (60)
+    2. error (50) 
+    3. warn (40)
+    4. info (30)
+    5. debug (20)
+    6. trace (10)
+
+    So if we are at a level then we will log everything above that level including that level. We will not log anything below that level.
+
+    eg. if we are at 'error' level then we will log error and fatal only and not log anything else below error level.
+*/
+// ========================================= //
+
 const log = bunyan.createLogger({
     name : 'test',
     streams : [
